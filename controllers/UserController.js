29c423ef819745685221c63defec6e35547a4353
Ivan Sotelo <ivan-.-sotelo@hotmail.com>
2017-10-31 22:05:01 -0700

    User controller

---
+++ controllers/UserController.js
@@ -0,0 +1,51 @@
+'use strict'
+var bcrypt = require('bcrypt-nodejs');
+var User = require('../models/User');
+
+/**
+ * Store a newly created resource in storage.
+ *
+ * @return Response
+ */
+
+function store(request, response) {
+  if (request.body.name && request.body.password && request.body.surname && request.body.email) {
+    var user = new User();
+    user.name = request.body.name;
+    user.surname = request.body.surname;
+    user.email = request.body.email;
+    user.role = 'ROLE_USER';
+    user.image = null;
+
+    User.findOne({email: user.email.toLowerCase()}, (err, issetUser) => {
+      if (err) {
+        response.status(500).send({message: 'Error al comprobar usuario'});
+      }else {
+        if (!issetUser) {
+          bcrypt.hash(request.body.password, null, null, function(err,hash){
+            user.password = hash;
+            user.save((err, userStored) => {
+              if (err){
+                response.status(500).send({message: 'Error al guardar usuario'});
+              }else {
+                if (!userStored) {
+                  response.status(404).send({message: 'Usuario no resgistrado'});
+                }else {
+                  response.status(200).send({user: userStored});
+                }
+              }
+            });
+          });
+        }else {
+          response.status(200).send({message: 'El usuario ya existe'});
+        }
+      }
+    });
+  }else {
+    response.status(200).send({message: 'Faltan campos para el registro'});
+  }
+}
+
+module.exports = {
+  store
+};
