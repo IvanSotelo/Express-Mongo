f2dbf78c410aff785c276931ee34c7ffa73d7a1f
Ivan Sotelo <ivan-.-sotelo@hotmail.com>
2017-10-31 22:04:18 -0700

    Creación de modelos

---
+++ models/User.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({
+  name: String,
+  surname: String,
+  email: String,
+  password: String,
+  role: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
