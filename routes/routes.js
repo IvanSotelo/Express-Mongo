f4093550d0a0257695c2a79eb009b6cbb14b870e
Ivan Sotelo <ivan-.-sotelo@hotmail.com>
2017-11-01 23:41:59 -0700

    Import middleware on routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,9 @@ var express = require('express');
 var UserController = require('../controllers/UserController');
 
 var Route = express.Router();
+var middleware = require('../middlewares/Authenticate');
 
 Route.post('/register', UserController.store);
-
+Route.get('/user',middleware.Auth, UserController.index);
+Route.put('/user/:id',middleware.Auth, UserController.update);
 module.exports = Route;
